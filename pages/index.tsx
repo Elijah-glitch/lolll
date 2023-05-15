d35feedffb1c315507b7df36571cd20b2736f6ce
Elijah-glitch <62041760+Elijah-glitch@users.noreply.github.com>
2023-05-15 05:24:31 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,16 +54,15 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             <div className="absolute inset-0 flex items-center justify-center opacity-20">
               <span className="flex max-h-full max-w-full items-center justify-center">
               </span>
-              <span className="absolute left-0 right-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
+              <span className=""></span>
             </div>
             <Logo />
-            <h1 className="mt-8 mb-4 text-base font-bold uppercase tracking-widest">
-              2022 Event Photos
+            <h1 className="">
             </h1>
-            <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
+            <p className="">
             </p>
             <a
-              className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
+              className=""
               href=""
               target="_blank"
               rel="noreferrer"
