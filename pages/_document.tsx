89d639e6e78ae2b07747472846c28694d5270666
Elijah-glitch <62041760+Elijah-glitch@users.noreply.github.com>
2023-05-15 05:38:24 -0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Tcapsmovies photos and the Archives."
+            content="See pictures from Next.js Conf and the After Party."
           />
-          <meta property="og:site_name" content="Discordgg.vercel.app" />
+          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
           <meta
             property="og:description"
-            content="See pictures from Tcapsmovies photos and the Archives."
+            content="See pictures from Next.js Conf and the After Party."
           />
-          <meta property="og:title" content="Tcapsmovies 2023 Pictures" />
+          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Tcapsmovies 2023 Pictures" />
+          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
           <meta
             name="twitter:description"
-            content="See pictures from Tcapsmovies photos and the Archives."
+            content="See pictures from Next.js Conf and the After Party."
           />
         </Head>
         <body className="bg-black antialiased">
